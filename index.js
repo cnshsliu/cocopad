79e5da8b52bcbacc1832cfbe07064eeb137a8d5d
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2020-02-28 10:54:24 +0800

    performance impr by moving nodes between layers

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin, ListGroupPlugin } from 'bootstrap-vue';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
 import "./scss/custom.scss";
 import KFK from '/src/console';
-// import 'custom.scss'; 
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -20,7 +17,7 @@ const app = new Vue({
     showCREATEROOM: false,
     showINVITATION: false,
     lockMode: KFK.lockMode,
-    icons: KFK.images,
+    images: KFK.images,
     active: {'tip': false, 'blanket':false, 'p8star':false, 'pin':false},
   },
 }).$mount("#app");
