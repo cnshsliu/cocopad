b685d1fb57980259907edd88b4801b5873f31494
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2020-03-01 02:06:15 +0800

    work like charming

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = new Vue({
     showINVITATION: false,
     lockMode: KFK.lockMode,
     images: KFK.images,
-    active: {'tip': false, 'blanket':false, 'p8star':false, 'pin':false},
+    active: {'tip': false, 'blanket':false, 'p8star':false, 'pin':false, 'text':false, 'yellowtip':false},
   },
 }).$mount("#app");
 KFK.APP = app;
