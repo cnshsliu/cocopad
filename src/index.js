23200fd69460f5d6431ef407c1c29649dc5b923d
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2020-03-09 02:19:28 +0800

    align, grid, snap

--- src/index.js
+++ src/index.js
@@ -18,18 +18,48 @@ const app = new Vue({
     showINVITATION: false,
     lockMode: KFK.lockMode,
     images: KFK.images,
-    toggle: {
-      line: false,
+    toggle: { line: false, },
+    active: { 'pointer': true, 'tip': false, 'blanket': false, 'p8star': false, 'pin': false, 'text': false, 'yellowtip': false, 'line': false, 'textblock': false },
+    show: {
+      'arrange_multi_nodes': false,
+      'shape_property': false,
     },
-    active: { 'tip': false, 'blanket': false, 'p8star': false, 'pin': false, 'text': false, 'yellowtip': false, 'line': false, 'textblock': false },
+    model: {
+      rightTabIndex: 0,
+      gridWidth: 20,
+      snap: true,
+      oldSnap: true,
+      showGrid: true,
+    }
   },
 }).$mount("#app");
 KFK.APP = app;
+app.setData = function (data, key, value) {
+  this.$set(this[data], key, value);
+};
+app.KfkAlign = function (direction) {
+  KFK.alignNodes(direction);
+}
 app.toggle = function (key, value) {
   this.$set(this.toggle, key, value);
   if (key === 'line') {
     $('#tool_line').attr('src', this.toggle.line ? app.images['hvline'].src : app.images['line'].src);
   }
 };
+app.showGridChanged = function (checked) {
+  console.log(`showGrid ${checked}`);
+  if (!checked) {
+    app.setData('model', 'oldSnap', app.model.snap);
+    app.setData('model', 'snap', false);
+  }
+  else {
+    app.setData('model', 'snap', app.model.oldSnap);
+  }
+  KFK.showGridChanged(checked);
+}
+
+app.snapChanged = function (checked) {
+  console.log(`snap ${checked}`);
+}
 
 app.setMode = KFK.setMode;
