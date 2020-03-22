aa73db8521a7b2729e1827722c6eef5e96108df3
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2020-03-22 13:56:42 +0800

    ws reconnect, prj/doc management

--- src/pad.js
+++ src/pad.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Joi from '@hapi/joi';
 import { BootstrapVue, IconsPlugin, ListGroupPlugin, VBHoverPlugin } from 'bootstrap-vue';
 import events from 'events';
 
@@ -11,6 +12,7 @@ Vue.use(ListGroupPlugin);
 Vue.use(VBHoverPlugin);
 const app = new Vue({
   data: {
+    KFK: KFK,
     seen: true,
     modalShow: false,
     modal_title: '',
@@ -35,8 +37,30 @@ const app = new Vue({
       'customline': true,
       'waitingws': true,
       'wsready': false,
+      'loginform': false,
+      'explorer': false,
+      'newdocform': false,
+      'form': { newdoc: false, newprj: false, contentlist: true, bottomlinks: false },
     },
     model: {
+      newprjname: '',
+      newdocname: '',
+      docLoaded: false,
+      project: { prjid: '', name: '' },
+      lastrealproject: { prjid: '', name: '' },
+      document: { docid: '', name: '' },
+      user: { userid: '', name: '' },
+      contentTabIndex: 0,
+      listdocoption: {},
+      listprjoption: {},
+      docfields: [{ key: 'name', label: '名称' }, { key: 'owner', label: '发起人' }],
+      prjfields: [{ key: 'name', label: '名称' }, { key: 'operation', label: '操作' }],
+      prjwarning: '',
+      docs: [],
+      prjs: [],
+      perPage: 10,
+      currentPrjPage: 1,
+      currentDocPage: 1,
       rightTabIndex: 2,
       gridWidth: 20,
       snap: true,
@@ -72,6 +96,34 @@ const app = new Vue({
         { value: 'all', text: '列出全部' }
       ],
     }
+  },
+  computed: {
+    doccount() {
+      return this.model.docs.length;
+    },
+    prjcount() {
+      return this.model.prjs.length;
+    },
+    docNameState() {
+      const schema = Joi.string().regex(/^[a-zA-Z0-9_\u4e00-\u9fa5]{3,20}$/).required();
+      let str = this.model.newdocname;
+      console.log('validate[' + str + ']');
+      let { error, value } = schema.validate(str);
+      if (error === undefined)
+        return true;
+      else
+        return false;
+    },
+    prjNameState() {
+      const schema = Joi.string().regex(/^[a-zA-Z0-9_\u4e00-\u9fa5]{3,20}$/).required();
+      let str = this.model.newprjname;
+      console.log('validate[' + str + ']');
+      let { error, value } = schema.validate(str);
+      if (error === undefined)
+        return true;
+      else
+        return false;
+    },
   },
   methods: {
     setData(data, key, value) {
@@ -112,12 +164,6 @@ const app = new Vue({
     vertAlignChanged() {
       KFK.vertAlignChanged();
     },
-    fullScreen() {
-      KFK.toggleFullScreen();
-    },
-    toggleRight() {
-      KFK.toggleRightPanel();
-    },
     setTipVariant(tip) {
       KFK.setTipVariant(tip);
     },
@@ -127,9 +173,36 @@ const app = new Vue({
     changeSVGFill() {
       KFK.changeSVGFill();
     },
-    handleTipHover(tip){
+    handleTipHover(tip) {
       console.log('Hover on ' + tip);
       // $('#tipvariant_'+tip).style.visibility= "hidden";
+    },
+    clickPrjItem(item, index, button) {
+      console.log(item);
+      console.log(index);
+      this.$bvModal.msgBoxConfirm('删除项目:' + item.name, {
+        title: '请确认',
+        size: 'sm',
+        buttonSize: 'sm',
+        okVariant: 'danger',
+        okTitle: '确认',
+        cancelTitle: '取消',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      })
+        .then(value => {
+          if (value) {
+            for (let i = 0; i < this.model.prjs.length; i++) {
+              if (this.model.prjs[i].prjid === item.prjid) {
+                KFK.deletePrj(item.prjid);
+                this.model.prjs.splice(i, 1);
+                break;
+              }
+            }
+          }
+        })
+        .catch(err => { })
     },
   },
 }).$mount("#app");
--- src/ws.js
+++ src/ws.js
@@ -3,21 +3,31 @@ const WS = {};
 WS.ws = null;
 WS.url = "ws://localhost:5008/grume/wsquux";
 
-WS.sleep =  function (delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => { try { console.log('-------->return from sleep..'); resolve(1) } catch (e) { reject(0) } }, delay);
-    })
-};
-
-WS.start = async (onOpen, callback) => {
-    await WS.sleep(1000);
+WS.reconnectInterval = null;
+WS.reconnect = function reconnect() {
+    WS.start(WS.onOpen, WS.callback, 0);
+}
+WS.start = async (onOpen, callback, delay) => {
+    WS.onOpen = onOpen;
+    WS.callback = callback;
+    if(delay>0)
+        await new Promise(resolve => setTimeout(resolve, delay));
+    console.log('connecting....');
     WS.ws = new WebSocket(WS.url);
     WS.ws.onopen = function () {
-        console.info(`ws opened to ${WS.url}`);
+        // console.info(`ws opened to ${WS.url}`);
+        console.log('connected');
+        if (WS.reconnectInterval != null) {
+            clearInterval(WS.reconnectInterval);
+            WS.reconnectInterval = null;
+        }
         onOpen();
     };
     WS.ws.onclose = function () {
         console.info(`ws close to ${WS.url}`);
+        if (WS.reconnectInterval === null) {
+            WS.reconnectInterval = setInterval(WS.reconnect, 1000);
+        }
     };
     WS.ws.onmessage = function (e) {
         callback(e.data);
@@ -32,9 +42,15 @@ WS.closeSocket = () => {
 };
 
 
-
 WS.put = async (cmd, payload) => {
     payload.cmd = cmd;
-    await WS.ws.send(JSON.stringify(payload));
+    console.log('readystate=' + WS.ws.readyState);
+    let ret = false;
+    if (WS.ws.readyState === 1) {
+        await WS.ws.send(JSON.stringify(payload));
+        ret = true;
+    }
+    console.log("return " + ret);
+    return ret;
 };
 export default WS;
