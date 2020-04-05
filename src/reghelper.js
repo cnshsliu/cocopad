f5d34ecf90715a23368993fc7e7ced147df08a37
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2020-04-05 22:05:50 +0800

    email verification is okay

--- src/reghelper.js
+++ src/reghelper.js
@@ -1,23 +1,21 @@
-let txt1 = '<meta charset="utf-8"><img src="https://svgjs.com/docs/3.0/assets/images/logo-svg-js-01d-128.png" alt="SVG.js logo"></img>';
-let txt2 = 'http://localhost:1234/doc/5e831143d542dbad345e646a';
 'use strict';
-
+const chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const RegHelper = {
   removeMeta: (txt) => {
     let g = txt.match(/<meta[^>]*>(.*)/su);
-    if(g === null) return txt;
+    if (g === null) return txt;
     else return g[1];
   },
-  isUrl: (txt)=>{
-      let g = txt.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/);
-      if(g===null) return false;
-      else return g[1];
+  isUrl: (txt) => {
+    let g = txt.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/);
+    if (g === null) return false;
+    else return g[1];
   },
-  urlAddress: (txt)=>{
-    let g= txt.match(/^<a href="([^"]*")>([^<]*)<\/a>/);
+  urlAddress: (txt) => {
+    let g = txt.match(/^<a href="([^"]*")>([^<]*)<\/a>/);
     return g;
   },
-  getDocIdInUrl: (pathname)=>{
+  getDocIdInUrl: (pathname) => {
     if (pathname.match(/\/doc\/(.+)/)) {
       let doc_id = pathname.substring(pathname.lastIndexOf("/") + 1);
       return doc_id;
@@ -27,4 +25,5 @@ const RegHelper = {
   },
 
 };
-export {RegHelper};
+
+export { RegHelper };
